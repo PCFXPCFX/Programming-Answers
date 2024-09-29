a0c4940a2606d65c472bd5ba002fa5a30a151d9a
PCFXPCFX <73052391+PCFXPCFX@users.noreply.github.com>
2024-09-29 21:08:14 +0800

    Create A.c

---
+++ nowcoder/Round62/A.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char input[6];
+    scanf("%s",input);
+    printf("%c",input[1]);
+    printf("%c",input[0]);
+    printf("%c",input[2]);
+    printf("%c",input[3]);
+    printf("%c",input[4]);
+    return 0;
+}
