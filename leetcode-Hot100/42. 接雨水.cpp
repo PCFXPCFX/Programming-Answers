e3f014f80473f4254f7686ab1bf75e95f9e15b17
PCFXPCFX <pcfx@qq.com>
2024-11-07 14:57:14 +0800

    Update 42. 接雨水.cpp

--- leetcode-Hot100/42. 接雨水.cpp
+++ leetcode-Hot100/42. 接雨水.cpp
@@ -26,3 +26,28 @@ public:
         return ans; 
     }
 };
+//法二
+class Solution {
+public:
+    int trap(vector<int>& height) {
+    int leftMax=0,rightMax=0,left=0,right=(height.size()-1);
+    int ans=0;
+    while(left<=right)
+    {
+        leftMax = max(leftMax,height[left]);
+        rightMax = max(rightMax,height[right]);
+        if(leftMax<rightMax)
+        {
+            ans+=leftMax - height[left];
+            left++;
+        }
+        else
+        {
+            ans+= rightMax - height[right];
+            right--;
+        }
+
+    }
+    return ans;
+    }
+};
