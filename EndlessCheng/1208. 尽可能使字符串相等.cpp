69901778aa9ee50b8e4dc65217e6e2c7e9ad0cfd
PCFXPCFX <pcfx@qq.com>
2024-12-09 20:59:51 +0800

    Update 1208. 尽可能使字符串相等.cpp

--- EndlessCheng/1208. 尽可能使字符串相等.cpp
+++ EndlessCheng/1208. 尽可能使字符串相等.cpp
@@ -4,9 +4,10 @@ public:
         int sum=0;
         int left=0;
         int ans=0;
-        //我的思路还是 左右端点
-        //如果两个sum之差在maxcost范围内,则继续扩张,否则left++
+        //我的思路还是 枚举右端点,维护左断点
+        //如果两个子字符串的消耗sum,在maxcost范围内,则继续扩张,否则减去left上的消耗,left++
         //维护ans 为i-left+1的最大值;
+        //时间复杂度O(n),空间复杂度O(n);
         vector<int> def(s.size(),0);
     
         for(int i=0;i<s.size();i++)
